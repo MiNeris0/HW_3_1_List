14de49a9faacd7413ce993fb2605698a9e6195c0
MiNeris0 <seikolor@gmail.com>
2023-05-27 22:43:12 +0300

    First Commit.

---
+++ HW_3_1_List/HW_3_1_List/CustomCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_3_1_List
+{
+    public class CustomCollection<T>
+        where T : IEnumerable<T>, IComparable<T>
+    {
+       
+    }
+}
---
+++ HW_3_1_List/HW_3_1_List/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HW_3_1_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new CustomCollection<int>();
+            arr.AddRange();
+        }
+    }
+}
