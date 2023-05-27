8e9b7b7cb5228b33d163598b5729997d8a849cbf
MiNeris0 <seikolor@gmail.com>
2023-05-28 01:02:24 +0300

    Third version.

--- HW_3_1_List/HW_3_1_List/CustomCollection.cs
+++ HW_3_1_List/HW_3_1_List/CustomCollection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -7,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace HW_3_1_List
 {
-    public class CustomCollection<T>
-        where T : IEnumerable<T>, IComparable<T>
+    public class CustomCollection<T> : IEnumerable<T>
+        where T : IComparable<T>
     {
         private const int InitCapacity = 10;
-        private const int ResizeStep = 3;
+        private const int ResizeStep = 2;
 
         private T[] _list;
         private int _size;
@@ -86,6 +87,11 @@ namespace HW_3_1_List
 
         public void Sort()
         {
+            Array.Sort(_list, 0, _size);
+        }
+
+        public void Sort(IComparer<T> comparer)
+        {
             Array.Sort(_list);
         }
 
@@ -97,6 +103,68 @@ namespace HW_3_1_List
                 {
                     _list[i] = _list[i + 1];
                 }
+                else
+                {
+                    _list[i] = default(T);
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new CustomCollectionEnumerator(_list, _size);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private sealed class CustomCollectionEnumerator : IEnumerator<T>
+        {
+            private T[] _list;
+            private int _size;
+            private int _position = -1;
+
+            public CustomCollectionEnumerator(T[] array, int size)
+            {
+                _list = array;
+                _size = size;
+            }
+            public bool MoveNext()
+            {
+                if (_position < _size - 1)
+                {
+                    _position++;
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                _position = -1;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    if (_position == -1 || _position >= _size)
+                    {
+                        throw new InvalidOperationException();
+                    }
+
+                    return _list[_position];
+                }
+            }
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+                // No unmanaged resources used.
             }
         }
     }
